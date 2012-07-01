36f2f0426b677cdf435ffd6df3845cc2934ab852
Joanna Rogowska <j.rogowska89@gmail.com>
2012-07-01 15:55:56 +0200

    first commit

---
+++ simple/src/main/java/com/mytutorial/App.java
@@ -0,0 +1,13 @@
+package com.mytutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
